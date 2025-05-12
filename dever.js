0fe3b6b49e576d283bb0650451ff8055d912f4ac
Vitor de Pádua Moreira Justo <vitordepadua003@gmail.com>
2025-05-11 23:01:03 -0300

    Add files via upload
    Escreva um programa em JS que recebe 3 números inteiros, e retorna a média aritmética entre eles.

---
+++ dever.js
@@ -0,0 +1,12 @@
+// Função para calcular a média aritmética
+function calcularMedia(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3;
+  }
+  
+  // Exemplo de uso
+  const numero1 = 10;
+  const numero2 = 20;
+  const numero3 = 30;
+  
+  const media = calcularMedia(numero1, numero2, numero3);
+  conso
